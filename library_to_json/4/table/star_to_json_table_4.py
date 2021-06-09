9fc1750b7635bf3b142884c3ee72828d3027ed1c
Ben Gindi <t-begind@microsoft.com>
2021-06-09 15:44:15 +0300

    adding minus 1s tables

---
+++ library_to_json/4/table/star_to_json_table_4.py
@@ -0,0 +1,19 @@
+import math
+import os
+
+if __name__ == "__main__":
+    rows = 52
+    columns = 32
+    file = open("4.txt", 'r')
+    lines = file.readlines()
+    for i in range(1,rows + 1): # 0 1 2 ... 29
+        for j in range(1,columns*2 + 1): # 1 2 3 ... 20
+            if j % 2 == 0:
+                continue
+            if len(lines[i]) < j:
+                break
+            seatnum = int((i-1)*columns + math.ceil(j/2)) - 1
+            if lines[i][j] == '*':
+                print(f"\"{seatnum}\": {{\n\t\"seat_index\": {seatnum},\n\t\"state\": true,\n\t\"user\": \"\",\n\t\"startDate\": 0\n\t}},")
+            else:
+                print(f"\"{seatnum}\": {{\n\t\"seat_index\": null\n\t}},")
---
+++ library_to_json/minus1/table/and_to_json_table_minus1.py
@@ -0,0 +1,18 @@
+import math
+
+if __name__ == "__main__":
+    rows = 36
+    columns = 23
+    file = open("../minus1.txt", 'r')
+    lines = file.readlines()
+    for i in range(1,rows + 1): # 0 1 2 ... 36
+        for j in range(1,columns*2 + 1): # 0 2 4 ... 46
+            if j % 2 == 0:
+                continue
+            if len(lines[i]) < j:
+                break
+            seatnum = int((i-1)*columns + math.ceil(j/2)) - 1
+            if lines[i][j] == '&':
+                print(f"\"{seatnum}\": {{\n\t\"is_table\": true\n\t}},")
+            else:
+                print(f"\"{seatnum}\": null,")
